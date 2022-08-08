d2e5bec47300f11f1f0e4f4920adf1e47cd573d1
Hugo Cardoso <hugorafaelmc@gmail.com>
2022-08-08 13:43:58 -0300

    test: adicionado mais casos de testes para 'TodoItem'

--- src/test/components/TodoItem.test.ts
+++ src/test/components/TodoItem.test.ts
@@ -87,4 +87,70 @@ describe("TodoItem", () => {
       completed: false,
     } as Todo)
   });
+
+  test("Deve emitir o evento 'change' após pressionar 'enter'", async () => {
+    const wrapper = mount(TodoItem, {
+      props: {
+        todo: {
+          id: "1234",
+          text: "Teste",
+          completed: true,
+        } as Todo,
+      },
+    });
+
+    await wrapper.find(".view label").trigger("dblclick");
+
+    const input = wrapper.find(".edit");
+
+    await input.setValue("Teste 2");
+
+    await input.trigger("keyup.enter");
+
+    const event = wrapper.emitted().change[0] as any[];
+
+    expect(event[0]).toBeTruthy();
+    expect(event[0]).toMatchObject({
+      id: "1234",
+      text: "Teste 2",
+      completed: true,
+    } as Todo)
+  });
+
+  test("Deve emitir o evento 'delete'", async () => {
+    const wrapper = mount(TodoItem, {
+      props: {
+        todo: {
+          id: "1234",
+          text: "Teste",
+          completed: true,
+        } as Todo,
+      },
+    });
+
+    await wrapper.find(".destroy").trigger("click");
+
+    const event = wrapper.emitted().delete[0] as any[];
+
+    expect(event[0]).toBeTruthy();
+    expect(event[0]).toMatchObject({
+      id: "1234",
+      text: "Teste",
+      completed: true,
+    } as Todo)
+  });
+
+  test("Deve exibir o texto", async () => {
+    const wrapper = mount(TodoItem, {
+      props: {
+        todo: {
+          id: "1234",
+          text: "Teste",
+          completed: true,
+        } as Todo,
+      },
+    });
+
+    expect(wrapper.find(".view label").text()).toBe("Teste");
+  });
 });
